44dbe1f241821c6537d8d33fdccda2b24c72aec7
BEN ENGLISCH <EMAILBEN145@gmail.com>
2012-07-10 18:19:40 -0500

    initial work on generateSignatureBase method etc.

--- oauth.go
+++ oauth.go
@@ -1,6 +1,7 @@
 package twitter
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha1"
@@ -8,6 +9,7 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"sort"
 	"time"
 )
 
@@ -15,43 +17,79 @@ const (
 	authHeaderString = `OAuth oauth_consumer_key="%s", oauth_nonce="%s", oauth_signature="%s", oauth_signature_method="HMAC-SHA1", oauth_timestamp="%s", oauth_token="%s", oauth_version="1.0"`
 )
 
-var (
-	consumerKey      = "xvz1evFS4wEEPTGEFPHBog"
-	consumerSecret   = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"
-	oauthToken       = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"
-	oauthTokenSecret = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
-	nonceRegexp, _   = regexp.Compile("[^a-zA-Z0-9]")
-)
+var nonceRegexp, _ = regexp.Compile("[^a-zA-Z0-9]")
 
-func generateOAuthHeader() string {
+type RestMethod struct {
+	Url    string
+	Method string
+	Params map[string]string
+	Data   string
+}
+
+// Generates OAuth http header
+func (t *Twitter) generateOAuthHeader() string {
 	return fmt.Sprintf(authHeaderString,
-		percentEncode(consumerKey),
+		encode(t.consumerKey),
 		getNonce(),
-		percentEncode(generateSignature("hello")),
+		encode(t.generateOAuthSignature("hello")),
 		fmt.Sprintf("%d", time.Now().Unix()),
-		percentEncode(oauthToken),
+		encode(t.oauthToken),
 	)
 }
 
-func generateSignature(signatureBase string) string {
-	signingKey := fmt.Sprintf("%s&%s", consumerSecret, oauthTokenSecret)
+// Generates an OAuth signature base string to be signed
+func (t *Twitter) generateSignatureBase(m RestMethod) string {
+	var buffer bytes.Buffer
+
+	// write method and url to buffer
+	buffer.WriteString(m.Method + "&")
+	buffer.WriteString(encode(m.Url) + "&")
+
+	// sort map keys
+	sortedKeys := sortMapKeys(m.Params)
+
+	// write each parameter to buffer
+	for _, v := range sortedKeys {
+		buffer.WriteString(encode(fmt.Sprintf("%s=%s&", v, m.Params[v])))
+	}
+
+	buffer.WriteString(encode(m.Data))
+
+	// return string
+	return buffer.String()
+}
+
+// Returns []string of alphabetically sorted map keys
+func sortMapKeys(m map[string]string) []string {
+	keys := make([]string, len(m))
+	i := 0
+	for k, _ := range m {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// Generates an OAuth signature using signatureBase
+// and secret keys
+func (t *Twitter) generateOAuthSignature(signatureBase string) string {
+	signingKey := fmt.Sprintf("%s&%s", t.consumerSecret, t.oauthTokenSecret)
 	hmac := hmac.New(sha1.New, []byte(signingKey))
 
 	hmac.Write([]byte(signatureBase))
 	return base64.StdEncoding.EncodeToString(hmac.Sum(nil))
 }
 
-func percentEncode(str string) string {
+// Wrapper for url.QueryEscape
+func encode(str string) string {
 	return url.QueryEscape(str)
 }
 
+// Returns a Nonce value
 func getNonce() string {
 	var bytes = make([]byte, 32)
 	rand.Read(bytes)
 	enc := base64.StdEncoding.EncodeToString(bytes)
 	return nonceRegexp.ReplaceAllString(enc, "")
-}
-
-func OAuthHeader() string {
-	return generateOAuthHeader()
 }
