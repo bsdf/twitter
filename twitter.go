e84576e6565c299983a2434cd641704611b4ce02
BEN ENGLISCH <EMAILBEN145@gmail.com>
2013-03-18 23:58:47 -0500

    it\'s 2013

--- twitter.go
+++ twitter.go
@@ -1,5 +1,5 @@
 // bsdf/twitter: an implementation of the twitter api in Go
-// Copyright (C) 2012 bsdf
+// Copyright (C) 2012, 2013 bsdf
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
